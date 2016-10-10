956bc618c536e9639fb4be4df689c4fd78b2cd0f
Karlos G liberal <karlosgliberal@gmail.com>
2016-10-10 10:24:53 +0200

    añadidas clases y jugando con los rangos

--- sketch.js
+++ sketch.js
@@ -1,52 +1,81 @@
-var img;
-var smallPoint, largePoint;
-
-
-var myImg;
-var polygons;
-var pixelColors = [];
-var scanLine = 0;
-var regla = [];
 var colorThief = new ColorThief();
-var plano =[];
-var plano_objeto = {};
-var fr = 60;
-var c, width, height, polydraw, colorArr, vertices, voronoi, polygons, colorDominat, paleta;
+var incremento = 0;
+var incremento2 = 1000;
+var incremento3 = 2000;
+var incremento4 = 3000;
+var incremento5 = 4000;
+var canvas, polygons;
+var pol1, pol2, pol3, pol4, pol5, pol6, pol7, pol8;
+var polis = [];
+var rang;
+var pares;
+var numeroConguntos = 10;
+var simultaneamente;
+
+
 
 function preload() {
   polygons = loadJSON("plano5.json");
 }
 
 function setup() {
-  frameRate(fr)
-  c = createCanvas(1024, 768);
-  scanLine = 0;
+  canvas = createCanvas(1024, 768);
   smooth(2);
-  console.log(polygons[1]);
+  for (var i = 0; i < 4000; i++) {
+    polis[i] =new Voronoi(polygons[i]);
+  }
+
+  simultaneamente = polygons.length / numeroConguntos;
+  console.log(simultaneamente);
+  rang = _.range(1, polygons.length, simultaneamente);
+  pares = _.partition(rang, n => n % 2);
+  console.log(pares);
+
+
+  pol1 = new Conjunto(polis, 1, 500);
+  pol2 = new Conjunto(polis, 500, 1000);
+  pol3 = new Conjunto(polis, 1000, 1500);
 
 }
 
 function draw(){
-  strokeWeight(.3);
-  stroke(255);
-    let incremento = frameCount;
-    var color = polygons[incremento][0];
-    polygons[incremento].shift();
-    var singlegon = polygons[incremento];
+  pol1.pintar();
+  pol2.pintar();
+  pol3.pintar();
+}
+
+class Conjunto {
+  constructor(voronoi, inicio, fin) {
+    this.voronoi = voronoi;
+    this.inicio = inicio;
+    this.fin = fin;
+  }
+
+  pintar(){
+    if (this.inicio < this.fin) {
+      this.inicio = this.inicio + 1;
+      this.voronoi[this.inicio].display();
+    }
+  }
+}
+
+class Voronoi {
+  constructor(polygons) {
+    this.polygons = polygons;
+    this.color = polygons[0];
+  }
+
+  display(){
+    strokeWeight(.3);
+    stroke(255);
+    let singlegon = this.polygons;
     stroke(255);
     beginShape();
-    fill(color);
-    for (var k = 0, kL = singlegon.length; k < kL; k++){
+    fill(this.color);
+    //empezamos en 1 para oviar el color
+    for (let k = 1, kL = singlegon.length; k < kL; k++){
       vertex(singlegon[k][0], singlegon[k][1]);
     }
     endShape(CLOSE);
-}
-
-function mousePressed() {
-  save("voronoi.jpg");
-}
-
-function mouseMoved(){
-  incremento = int(map(mouseX,0,1024,1,3999));
-  console.log(incremento);
+  }
 }
