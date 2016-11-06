9cfa0d313d066d6c9ca69a90376841f85d02cec5
Karlos G liberal <karlosgliberal@gmail.com>
2016-11-06 14:46:25 +0100

    deploy

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//The mars voyage karlos g liberal (@patxangas) http://interzonas.info 2
+//The mars voyage karlos g liberal (@patxangas) http://interzonas.info 3
 var canvas, polygons;
 var polis = [];
 var pol = []
