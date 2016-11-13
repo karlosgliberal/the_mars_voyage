23f677d3c983348200d59239bbb6131fc79b872d
Karlos G liberal <karlosgliberal@gmail.com>
2016-11-13 13:51:40 +0100

    deploy

--- build/sketch.js
+++ build/sketch.js
@@ -255,6 +255,7 @@ var Utils = function () {
       text("loading...", 200, 200);
 
       var cararTexto = setInterval(function () {
+        text("", 200, 200);
         text(alphabet[random(0, 24)], 200, 200);
       }, 40);
 
