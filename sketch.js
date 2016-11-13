9472f82f57f62589ab8489373196bd7a539583fd
Karlos G liberal <karlosgliberal@gmail.com>
2016-11-13 18:35:05 +0100

    deploy

--- sketch.js
+++ sketch.js
@@ -195,7 +195,7 @@ class Utils{
     let result = polygons.map(function(c, i) {
       return {color: Utils.rgbToHsl(c[0]), index: i};
     }).sort(function(c1, c2) {
-      return c1.color[2] - c2.color[2];
+      return c2.color[2] - c1.color[2];
     }).map(function(data) {
       return polygons[data.index];
     });
